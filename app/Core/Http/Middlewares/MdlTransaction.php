6dd83e830f65001e64221405d021e96bf729931f
bit-craft-io <bit.craft@outlook.com>
2025-11-13 13:35:55 +0900

    update: 20251113

--- app/Core/Http/Middlewares/MdlTransaction.php
+++ app/Core/Http/Middlewares/MdlTransaction.php
@@ -22,26 +22,26 @@ final class MdlTransaction
     {
         $response = $next($request);
 
-        $this->_DevLog::emergency('begin');
+        $this->_DevLog::info('begin');
 
         $tranLevel = $this->_Transaction::getLevel();
 
         // @note ロールバック
         if ($this->_Transaction::isRollback()) {
             for ($i = 0; $i < $tranLevel; $i++) {
-                $this->_DevLog::emergency('rollback');
+                $this->_DevLog::info('rollback');
                 $this->_Transaction::rollback();
             }
-            $this->_DevLog::emergency('end');
+            $this->_DevLog::info('end');
             return $response;
         }
 
         // @note コミット
         for ($i = 0; $i < $tranLevel; $i++) {
-            $this->_DevLog::emergency('commit');
+            $this->_DevLog::info('commit');
             $this->_Transaction::commit();
         }
-        $this->_DevLog::emergency('end');
+        $this->_DevLog::info('end');
         return $response;
     }
 }
